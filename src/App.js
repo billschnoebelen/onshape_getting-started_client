3d1ffb5e0697ac78464a0b2f5e689fa96520b372
Bill Schnoebelen <Bill@circuitcase.com>
2020-10-26 11:20:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [stl, setStl] = useState("");
+
+  useEffect(() => {
+    const headers = {
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+    };
+
+    async function getSTL() {
+      try {
+        const response = await axios.get("http://localhost:8000/stl", headers);
+        console.log(response);
+        setStl(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    getSTL();
+  }, []);
+
+  return <div className="App">{stl}</div>;
 }
 
 export default App;
